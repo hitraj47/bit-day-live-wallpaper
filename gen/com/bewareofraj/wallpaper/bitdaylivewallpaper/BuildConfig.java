d7e98cf44f983828f40b18d6605bb08e4e890507
Rajesh <Rajesh>
2016-05-04 01:01:23 -0400

    update project to run on android studio

--- gen/com/bewareofraj/wallpaper/bitdaylivewallpaper/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.bewareofraj.wallpaper.bitdaylivewallpaper;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
